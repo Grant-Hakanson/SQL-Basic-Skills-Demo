280149661a33b702eb3bbd0d3883f4121b350ace
Grant-Hakanson <91210134+Grant-Hakanson@users.noreply.github.com>
2021-10-15 03:37:30 -0400

    Add files via upload

--- IBMCaseColumnFormating.sql
+++ IBMCaseColumnFormating.sql
@@ -1,8 +1,108 @@
--- Table Formating using case statements. See IBM Tableau Project on Tableau Public page, User Grant Hakanson.
+-- Table Formating using case statements. Converting Meausre feilds into Dimensional Feilds.
+-- See IBM Tableau Project onTableau Public page, User Grant Hakanson.
 
+-- Converting Data Ranges Into Nominal Values
 
+-- Data Range conversion Column 1
+alter table dbo.ibmdata
+add Monthlyincome_Dim as 
+(Case
+	When monthlyincome between 1009 and 3018 then 'Very Low Income'
+	when monthlyincome between 3018 and 6856 then 'Low Income'
+	when monthlyincome between 6856 and 15427 then 'Moderate Income'
+	When monthlyincome between 15427 and 17713 then 'High Income'
+	when monthlyincome between 17714 and 19999 then 'Very High Income'
+	end)
+	
+	
+------------------------------------------------------------------------------------------------------------------------------------
+-- Data Range conversion Column 2
+alter table dbo.ibmdata
+add Percent_Salary_Increase_Dim as 
+(Case
+	When percentsalaryhike between 11 and 13 then 'Very Low Increase'
+	when percentsalaryhike between 13 and 15 then 'Low Increase'
+	when percentsalaryhike between 15 and 17 then 'Moderate Increase'
+	When percentsalaryhike between 17 and 20 then 'High Increase'
+	when percentsalaryhike between 20 and 25 then 'Very High Increase'
+	end)
+-----------------------------------------------------------------------------------------------------------------------------------	
+-- Data Range conversion Column 3
 
--- New Column 1
+alter table dbo.ibmdata
+add DistanceFromHome_dim as 
+(Case
+	When distancefromhome between 1 and 5 then 'Very Short Distance'
+	when distancefromhome between 5 and 9 then 'Short Distance'
+	when distancefromhome between 9 and 15 then 'Moderate Distance'
+	When distancefromhome between 15 and 22 then 'Long Distance'
+	when distancefromhome between 22 and 29 then 'Very Long Distance'
+	end)
+----------------------------------------------------------------------------------------------------------------------------------
+-- Data Range conversion Column 4
+
+alter table dbo.ibmdata
+add AgeRange_Dim as 
+(Case
+	When Age between 18 and 26 then '18-26yrs'
+	when age between 26 and 34 then '26-34yrs'
+	when age between 34 and 42 then '34-42yrs'
+	When age between 42 and 50 then '42-50yrs'
+	when age between 50 and 60 then '50-60yrs'
+	end)
+
+-----------------------------------------------------------------------------------------------------------------------------------
+	-- Data Range conversion Column 5
+
+alter table dbo.ibmdata
+add Totalworkingyears_Dim as 
+(Case
+	When Totalworkingyears between 0 and 8 then '0-8yrs'
+	when Totalworkingyears between 8 and 16 then '8-16yrs'
+	when Totalworkingyears between 16 and 24 then '16-24yrs'
+	When Totalworkingyears between 24 and 32 then '24-32yrs'
+	when Totalworkingyears between 32 and 40 then '32-40yrs'
+	end)
+-----------------------------------------------------------------------------------------------------------------------------------
+-- Data Range conversion Column 6
+
+alter table dbo.ibmdata
+add yearswithcurrmanager_Dim as 
+(Case
+	When yearswithcurrmanager between 0 and 4 then '0-4yrs'
+	when yearswithcurrmanager between 4 and 8 then '4-8yrs'
+	when yearswithcurrmanager between 8 and 12 then '8-12yrs'
+	When yearswithcurrmanager between 12 and 16 then '12-16yrs'
+	when yearswithcurrmanager between 16 and 20 then '16-20yrs'
+	end)
+-----------------------------------------------------------------------------------------------------------------------------------
+-- Data Range conversion Column 7
+
+alter table dbo.ibmdata
+add yearssincelastpromotion_Dim as 
+(Case
+	When yearssincelastpromotion between 0 and 3 then '0-3yrs'
+	when yearssincelastpromotion between 3 and 6 then '3-6yrs'
+	when yearssincelastpromotion between 6 and 9 then '6-9yrs'
+	When yearssincelastpromotion between 9 and 12 then '9-12yrs'
+	when yearssincelastpromotion between 12 and 15 then '12-15yrs'
+	end)
+----------------------------------------------------------------------------------------------------------------------------------
+	-- Data Range conversion Column 8
+
+alter table dbo.ibmdata
+add yearssincelastpromotion_Dim as 
+(Case
+	When yearssincelastpromotion between 0 and 3 then '0-3yrs'
+	when yearssincelastpromotion between 3 and 6 then '3-6yrs'
+	when yearssincelastpromotion between 6 and 9 then '6-9yrs'
+	When yearssincelastpromotion between 9 and 12 then '9-12yrs'
+	when yearssincelastpromotion between 12 and 15 then '12-15yrs'
+	end)
+
+-----------------------------------------------------------------------------------------------------------------------------------
+--Turing Numeric Values into Nominal Values
+-- Numeric to Nominal Column 1
 
 alter table dbo.ibmdata
 add Education_Dim as
@@ -15,7 +115,7 @@ add Education_Dim as
 	else 'null'
 end)
  ----------------------------------------------------------------------------------------------------------------------------------
--- Column 2
+-- Numeric to Nominal Column 2
 
 alter table dbo.ibmdata
 add EnvironmentalSatisfaction_Dim as
@@ -31,7 +131,7 @@ end)
 
 
 -----------------------------------------------------------------------------------------------------------------------------------
--- Column 3
+-- Numeric to Nominal Column 3
 
 alter table dbo.ibmdata
 add JobInvolvement_Dim as
@@ -45,7 +145,7 @@ add JobInvolvement_Dim as
 end)
 
 -----------------------------------------------------------------------------------------------------------------------------------
---Column 4
+-- Numeric to Nominal Column 4
 
 alter table dbo.ibmdata
 add JobLevel_Dim as
@@ -58,7 +158,7 @@ add JobLevel_Dim as
 	else 'null'
 end)
 ----------------------------------------------------------------------------------------------------------------------------------
--- Column 5
+-- Numeric to Nominal Column 5
 
 alter table dbo.ibmdata
 add JobSatisfaction_Dim as
@@ -71,7 +171,7 @@ add JobSatisfaction_Dim as
 	else 'null'
 end)
 -----------------------------------------------------------------------------------------------------------------------------------
--- Column 6
+-- Numeric to Nominal Column 6
 
 alter table dbo.ibmdata
 add PerformanceRating_Dim as
@@ -84,7 +184,7 @@ add PerformanceRating_Dim as
 	else 'null'
 end)
 -----------------------------------------------------------------------------------------------------------------------------------
--- Column 7
+-- Numeric to Nominal Column 7
 
 alter table dbo.ibmdata
 add RelationshipSatisfaction_Dim as
@@ -97,7 +197,7 @@ add RelationshipSatisfaction_Dim as
 	else 'null'
 end)
 ------------------------------------------------------------------------------------------------------------------------------------
--- Column 8
+-- Numeric to Nominal Column 8
 
 alter table dbo.ibmdata
 add WorkLifeBalance_Dim as
@@ -110,4 +210,14 @@ add WorkLifeBalance_Dim as
 	else 'null'
 end)
 
+-----------------------------------------------------------------------------------------------------------------------------------
+-- Numeric to Nominal Column 9
+alter table dbo.ibmdata
+add StockOptions_Dim as 
+(Case
+	When stockoptionlevel = 0 then 'No Options'
+	when stockoptionlevel = 1 then 'Low Options'
+	when stockoptionlevel = 2 then 'Moderate Options'
+	when stockoptionlevel = 3 then 'Many Options' 
+end)
 
